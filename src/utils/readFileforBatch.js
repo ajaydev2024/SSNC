d73e16903ef42a60bb71632e993695c39691e4a7
ajaydev2024 <ajaysagarbaghel@gmail.com>
2023-08-18 12:20:39 +0530

    Fifth Repo

--- src/utils/readFileforBatch.js
+++ src/utils/readFileforBatch.js
@@ -3,9 +3,9 @@ import fs from 'fs/promises';
 
 const readJSFile = async (filePath) => {
   try {
-   const fileContent = await fs.readFile(filePath, 'utf-8');
-   
+    const fileContent = await readJSFile(filePath);   
     return fileContent;
+    
   } catch (error) {
     console.error('Error reading file:', error);
     throw error;
