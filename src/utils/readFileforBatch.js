1c005629ac3bd28ca2b48654f9179ad9ab1a4d0e
ajaydev2024 <ajaysagarbaghel@gmail.com>
2023-08-18 12:09:27 +0530

    Four repo

--- src/utils/readFileforBatch.js
+++ src/utils/readFileforBatch.js
@@ -3,10 +3,8 @@ import fs from 'fs/promises';
 
 const readJSFile = async (filePath) => {
   try {
-  // const fileContent = await fs.readFile(filePath, 'utf-8');
-   // eslint-disable-next-line eslint-disable-line @typescript-eslint/no-floating-promises
-const fileContent = await readJSFile(filePath);
-
+   const fileContent = await fs.readFile(filePath, 'utf-8');
+   
     return fileContent;
   } catch (error) {
     console.error('Error reading file:', error);
