f382f005c04d93bdb27a436c7d4883bfabc803b3
ajaydev2024 <ajaysagarbaghel@gmail.com>
2023-08-18 18:08:47 +0530

    14th Repo

--- src/utils/readFileforBatch.js
+++ src/utils/readFileforBatch.js
@@ -1,15 +1,14 @@
-// src/utils/readFileforBatch.js
 import fs from 'fs/promises';
-import path from 'path';
 
 const readJSFile = async (filePath) => {
   try {
     // eslint-disable-next-line node/no-template-curly-in-string
-    const fileContent = await fs.readFile(path.join(filePath), 'utf-8');
+    const fileContent = await fs.readFile(filePath, 'utf-8');
     return fileContent;
   } catch (error) {
     console.error('Error reading file:', error);
   }
+  
 };
 
 export default readJSFile;
