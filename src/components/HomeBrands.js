0a2529990f4e73dcb6c84db0d43de36beb17d687
ajaydev2024 <ajaysagarbaghel@gmail.com>
2023-08-18 16:50:31 +0530

    Tenth repo

--- src/components/HomeBrands.js
+++ src/components/HomeBrands.js
@@ -13,13 +13,13 @@ const HomeBrands = () => {
     };
 
     return (
-        <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-4">
-            <div className="text-center pb-2">
-                <h2 className="text-base font-bold text-blue-600">
+        <section className="max-w-6xl mx-auto px-4 sm:px-6  py-4">
+            <div className="text-center pb-2 mt-20 lg:mt-0 lg:px-4">
+                <h2 className="text-sm sm:text-sm md:text-md lg:text-2xl   font-bold text-blue-600">
                     Click on Any Supplement Brands to Shows its Items
                 </h2>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+            <div className="grid grid-cols-3  lg:grid-cols-5 gap-6">
                 {/* Replace with your brand data */}
                 {brands.map((brand, index) => (
                     <div
@@ -27,7 +27,7 @@ const HomeBrands = () => {
                         onClick={() => handleBrandClick(brand.name)}
                         className="w-full bg-gray-900 rounded-lg p-12 flex flex-col justify-center items-center hover:scale-110 shadow-lg cursor-pointer"
                     >
-                        <div className="mb-8">
+                        <div className="mb-8 ">
                             <Image
                                 className="object-center object-cover"
                                 src={brand.imageSrc}
@@ -39,7 +39,7 @@ const HomeBrands = () => {
                             />
                         </div>
                         <div className="text-center">
-                            <p className="text-xl text-white font-bold mb-2">{brand.name}</p>
+                            <p className="text-sm sm:text-sm  md:text-md lg:text-xl  text-white font-bold mb-2">{brand.name}</p>
                         </div>
                     </div>
                 ))}
